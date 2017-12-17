ed307fa1cc3171256491f98acbd2699c460fd48d
zashim <zashim@192.168.2.8>
2017-12-17 17:27:31 -0500

    pushy

--- Lab 4.1 Cards/src/Deck.java
+++ Lab 4.1 Cards/src/Deck.java
@@ -7,10 +7,42 @@ public class Deck
 	
 	public Deck(String[] rank,String[] suit,int[] pointValue)
 	{
-		this.unDealt;
 		for(int i = 0; i < rank.length; i++)
 		{
-			
+			unDealt.add(new Card(rank[i],suit[i],pointValue[i]));
 		}
+	}
+	public boolean isEmpty()
+	{
+		if(unDealt.size() == 0)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	public int size()
+	{
+		return unDealt.size();
+	}
+	public Card deal()
+	{
+		
+		for(int i = 0; i < unDealt.size(); i++)
+		{
+			if(unDealt.size() != 0)
+			{
+				Card x = unDealt.get(i);
+				unDealt.remove(i);
+				dealt.add(x);
+				return x;
+			}
+		}
+		return null;
+	}
+	public void shuffle()
+	{
 	}
 }
